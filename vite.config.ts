62ba2694321d253e3d38a0816fbe000f233fc4c6
Ludovico Cannizzaro <148359279+Lud000v1c0@users.noreply.github.com>
2025-11-27 23:54:02 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // AGGIUNGI QUESTA RIGA:
+  base: '/cortocircuitosite/', 
+})
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
